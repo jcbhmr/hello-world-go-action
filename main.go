93d5a01fa25797243a25be0fd7a5de58a540fda4
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 18:38:08 +0000

    try this

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Printf("Hello %s!\n", "jcbhmr")
-	fmt.Printf("%s=%s", "time", time.Now().Format("3:04 PM"))
+	color.Set(color.FgRed)
+	defer color.Unset()
+	fmt.Printf("Hello %s!\n", "world")
+	color.Set(color.FgBlue)
+	fmt.Printf("%s=%s\n", "time", time.Now().Format("3:04 PM"))
 }
