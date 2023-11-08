a695a869d6257873b4f60428bc2e74965faeec7c
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 12:03:15 -0600

    mkdir

--- _main.mjs
+++ _main.mjs
@@ -2,6 +2,7 @@ import { spawn } from "node:child_process";
 import { once } from "node:events";
 import { join, dirname } from "node:path";
 import { existsSync, createWriteStream } from "node:fs";
+import { mkdir } from "node:fs/promises";
 const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
 const response1 = await fetch("https://go.dev/dl/?mode=json&include=all");
 const json = await response1.json();
@@ -9,6 +10,7 @@ const tag = json.map((x) => x.version).find((x) => x.startsWith("go1."));
 const version = tag.slice(2);
 const DEST = join(process.env.RUNNER_TOOL_CACHE, "go", version, process.arch);
 if (!existsSync(DEST)) {
+  await mkdir(DEST);
   const platform = {
     darwin: "darwin",
     linux: "linux",
@@ -21,7 +23,7 @@ if (!existsSync(DEST)) {
   const ext = platform === "windows" ? "zip" : "tar.gz";
   const filename = `${tag}.${platform}-${arch}.${ext}`;
   const response2 = await fetch(`https://go.dev/dl/${filename}`);
-  const SRC = join(install, filename);
+  const SRC = join(DEST, filename);
   await pipeline(response2.body, createWriteStream(SRC));
   if (filename.endsWith(".zip")) {
     const subprocess1 = spawn("tar", ["-f", SRC, "-C", DEST]);
