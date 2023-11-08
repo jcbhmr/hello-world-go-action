9d0e0e88d6db03d5dc1a9cc59cf9d5404c497f1f
Jacob Hummer <jcbhmr@outlook.com>
2023-11-08 11:51:46 -0600

    Update _main.mjs

--- _main.mjs
+++ _main.mjs
@@ -1,14 +1,14 @@
 import { spawn } from "node:child_process";
 import { once } from "node:events";
 import { join, dirname } from "node:path";
-import { existsSync } from "node:fs";
+import { existsSync, createWriteStream } from "node:fs";
 const file = join(dirname(process.argv[1]), "main.ts"); // 👈 CHANGE ME!
 const response1 = await fetch("https://go.dev/dl/?mode=json&include=all");
 const json = await response1.json();
 const tag = json.map((x) => x.version).find((x) => x.startsWith("go1."));
 const version = tag.slice(2);
-let install = join(process.env.RUNNER_TOOL_CACHE, "go", version, process.arch);
-if (!existsSync(install)) {
+const DEST = join(process.env.RUNNER_TOOL_CACHE, "go", version, process.arch);
+if (!existsSync(DEST)) {
   const platform = {
     darwin: "darwin",
     linux: "linux",
@@ -21,26 +21,20 @@ if (!existsSync(install)) {
   const ext = platform === "windows" ? "zip" : "tar.gz";
   const filename = `${tag}.${platform}-${arch}.${ext}`;
   const response2 = await fetch(`https://go.dev/dl/${filename}`);
-  await pipeline(response2.body, createWriteStream(join(install, filename)))
+  const SRC = join(install, filename)
+  await pipeline(response2.body, createWriteStream(SRC))
+  if (filename.endsWith(".zip")) {
+    const subprocess1 = spawn("tar", ["-f", SRC, "-C", DEST]);
+    await once(subprocess1, "exit");
+  } else {
+    const subprocess2 = spawn(`Expand-Archive -Path $Env:SRC -Destination $Env:DEST`, { shell: "powershell", env: { ...process.env, SRC, DEST } });
+    await once(subprocess2, "exit");
+  }
 }
-
-const DENO_INSTALL = join(
-  process.env.RUNNER_TOOL_CACHE,
-  "deno",
-  version,
-  process.arch
-);
-if (!existsSync(DENO_INSTALL)) {
-  const subprocess1 = spawn(
-    `curl -fsSL https://deno.land/x/install/install.sh | sh -s "$tag"`,
-    { shell: "bash", env: { ...process.env, DENO_INSTALL, tag } }
-  );
-  await once(subprocess1, "exit");
-}
-const subprocess2 = spawn(
-  join(DENO_INSTALL, "bin", "deno"),
-  ["run", "-Aq", file],
+const subprocess3 = spawn(
+  join(DEST, "bin", "go"),
+  ["run", file],
   { stdio: "inherit" }
 );
-await once(subprocess2, "spawn");
-subprocess2.on("exit", (x) => process.exit(x));
+await once(subprocess3, "spawn");
+subprocess3.on("exit", (x) => process.exit(x));
